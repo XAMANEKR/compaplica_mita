3b1efbb411eebc937e172b28a2ed0efb4c3671f4
XAMANEKR <xamanek.rocha@gmail.com>
2022-09-25 12:35:54 -0500

    k

---
+++ p43-num-may.py
@@ -0,0 +1,12 @@
+
+
+lista=[]
+while True:
+    n=int(input("Humano ingresa cualquier numero (0 para terminar):"))
+    if n==0:
+            break
+    else:
+        lista.append(n)
+
+    
+print(f"mayor={mayor},menor={menor}")

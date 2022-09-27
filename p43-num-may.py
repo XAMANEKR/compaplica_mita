06c4ffa6b2b41b93a8d160cb168d345b1acf92ca
XAMANEKR <xamanek.rocha@gmail.com>
2022-09-27 18:16:50 -0500

    1while faltante

--- p43-num-may.py
+++ p43-num-may.py
@@ -1,9 +1,16 @@
+total=int(input("escribe el total de num a calcular"))
 
+while (True):
+    x=1
+    
+    while x<= total:
+        n=int(input("escribe un numero"))
+        numero_mayor = n
+        x=x+1
+        if x == 1:
+            numero_mayor=n
+        elif n > numero_mayor:
+            numero_mayor=n
 
-lista=[]
-while True:
-    n=int(input("Humano ingresa cualquier numero (0 para terminar):"))
-    if n==0:
-            break
-    else:
-        lista.append(n)
+    
+    print(f"el numero mayor es:{numero_mayor}")
--- p59-sec-n-terminos.py
+++ p59-sec-n-terminos.py
@@ -2,8 +2,11 @@ print(" imprimir la secuencia de números mostrados el numero de renglones que e
 
 print("impirme triangulo de renglones")
 n = int(input("Cuantos renglones ? "))
-
-for i in range (1+n+1):
-    for j in range (1,i):
-
-        print(j,end=(" "))
+C=1
+for i in range (1,n+1):
+   
+     for j in range (1,i):
+       
+      print(C,end=(""))
+     
+           

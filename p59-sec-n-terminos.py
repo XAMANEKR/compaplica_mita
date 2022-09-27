469281f6378c936c6631a96d2f817231ff6a3fc0
XAMANEKR <xamanek.rocha@gmail.com>
2022-09-27 15:20:09 -0500

    c

---
+++ p59-sec-n-terminos.py
@@ -0,0 +1,9 @@
+print(" imprimir la secuencia de números mostrados el numero de renglones que el usuario desee")
+
+print("impirme triangulo de renglones")
+n = int(input("Cuantos renglones ? "))
+
+for i in range (1+n+1):
+    for j in range (1,i):
+
+        print(j,end=(" "))
